51d027c536ce3e6f68805489e006c553a10a81e9
Ali Ahmadi <Ali_Ahmadi_@outlook.com>
2022-10-04 16:25:32 +0330

    services updated

--- src/services.js
+++ src/services.js
@@ -51,7 +51,7 @@ export default function services(getState,token,userCardCode) {
 
           const userRequestCount=users.filter(x=>x.userId===u.userId).length;
           return {requestCount:userRequestCount,requestDate:u.createdDate,amount:u.amount,darkhasteBardashtStatus:'0',userId:u.userId};
-        })
+        });
 
         // return [
         //     {requestCount:1,requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0',userId:'0'},
@@ -64,74 +64,111 @@ export default function services(getState,token,userCardCode) {
         //     {requestCount:1,requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0',userId:'4'},
         // ]
       },
-      async darkhaste_bardasht_history({parameter}){
+      async darkhaste_bardasht_history({baseUrl,parameter}){
         let {userId} = parameter;
-        return [
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
-            {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'}
-          ]
+        let result = await Axios.get(`${baseUrl}/WithdrawRequest/WithFilter?filters=UserId==${userId}`);
+        if(!result.data.isSuccess) return;
+        
+        return result.data.data.map(x=>{
+          return {requestDate:x.createdDate,amount:x.amount,darkhasteBardashtStatus:'0'};
+        });
+        // return [
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'},
+        //     {requestDate:'1401/4/5 12:20',amount:1234567,darkhasteBardashtStatus:'0'}
+        //   ]
       },
-      async tarikhche_gardoone(){
-        return [
-            {winCount:1,tryCount:12,gardooneStatus:true,userId:'0'},
-            {winCount:1,tryCount:12,gardooneStatus:false,userId:'1'},
-            {winCount:1,tryCount:12,gardooneStatus:true,userId:'2'},
-            {winCount:1,tryCount:12,gardooneStatus:false,userId:'3'},
-            {winCount:1,tryCount:12,gardooneStatus:true,userId:'4'}
-        ]
+      async tarikhche_gardoone({baseUrl}){
+
+        let result = await Axios.get(`${baseUrl}/UserAwards/WithFilter`);
+        if(!result.data.isSuccess) return;
+        
+        const slotData=result.data.data;
+        return slotData.map(x=>{
+          
+          const userTryCount=slotData.filter(i=>i.userId===x.userId);
+          const winCount=userTryCount.filter(i=>i.win).length;
+          return {winCount,tryCount:userTryCount.length,gardooneStatus:x.user.isSlotMachineActive,userId:x.userId};
+        });
+
+        // return [
+        //     {winCount:1,tryCount:12,gardooneStatus:true,userId:'0'},
+        //     {winCount:1,tryCount:12,gardooneStatus:false,userId:'1'},
+        //     {winCount:1,tryCount:12,gardooneStatus:true,userId:'2'},
+        //     {winCount:1,tryCount:12,gardooneStatus:false,userId:'3'},
+        //     {winCount:1,tryCount:12,gardooneStatus:true,userId:'4'}
+        // ]
       },
-      async tarikhche_javayeze_barande_shode({parameter}){
+      async tarikhche_javayeze_barande_shode({parameter,baseUrl}){
         let {userId} = parameter;
-        return [
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'},
-            {date:'1401/4/5 12:20',award:'جایزه 1'}
-          ]
+        let result = await Axios.get(`${baseUrl}/UserAwards/WithFilter?filters=UserId==${userId}`);
+        if(!result.data.isSuccess) return;
+        
+        return result.data.data.map(x=>{
+          return {date:x.createdDate,award:x.award.title};
+        });
+        // return [
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'},
+        //     {date:'1401/4/5 12:20',award:'جایزه 1'}
+        //   ]
       },
       async sefareshate_bazargah({baseUrl}){
 
-        // let res = await Axios.get(`${baseUrl}/OS/GetWithDistance?cardCode=${userCardCode}&distance=100&status=00`);
+        let result = await Axios.get(`${baseUrl}/OS/GetWithDistance?cardCode=${"c39832"}&distance=100&status=0&time=100000`);
+        if(!result.data.isSuccess) return;
 
-        return [
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'0',"latitude": 35.699739,"longitude": 51.338097},
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'1',"latitude": 35.699739,"longitude": 51.338097},
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'2',"latitude": 35.699739,"longitude": 51.338097},
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'3',"latitude": 35.699739,"longitude": 51.338097},
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'4',"latitude": 35.699739,"longitude": 51.338097},
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'1',"latitude": 35.699739,"longitude": 51.338097},
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'2',"latitude": 35.699739,"longitude": 51.338097},
-            {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'3',"latitude": 35.699739,"longitude": 51.338097}
-          ]
+        const data=result.data.data;
+        return data.map(x=>{
+          return {id:x.id,orderNumber:x.orderNumber,orderDate:x.orderDate,firstname:x.receiverName,lastname:x.receiverName,state:x.province,city:x.city,mobile:x.receiverNumber,orderAmount:x.finalAmount,orderStatus:'0',latitude: x.latitude,longitude: x.longitude};
+        });
+
+        // return [
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'0',"latitude": 35.699739,"longitude": 51.338097},
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'1',"latitude": 35.699739,"longitude": 51.338097},
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'2',"latitude": 35.699739,"longitude": 51.338097},
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'3',"latitude": 35.699739,"longitude": 51.338097},
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'4',"latitude": 35.699739,"longitude": 51.338097},
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'1',"latitude": 35.699739,"longitude": 51.338097},
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'2',"latitude": 35.699739,"longitude": 51.338097},
+        //     {orderNumber:'4324',orderDate:'1400/3/3',firstname:'محمد',lastname:'فیض',state:'تهران',city:'تهران',mobile:'09123534314',orderAmount:12345678,orderStatus:'3',"latitude": 35.699739,"longitude": 51.338097}
+        //   ]
       },
-      async tarikhche_taghire_vaziate_bazargah({parameter}){
+      async tarikhche_taghire_vaziate_bazargah({baseUrl,parameter}){
         let {order} = parameter;
-        return [
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
-            {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'}
-          ]
+
+        let result = await Axios.get(`${baseUrl}/OSOrderLog/WithFilter?filters=OsOrderId==${order.id}`);
+        if(!result.data.isSuccess) return;
+        return result.data.data.map(x=>{
+          return {date:x.createdDate,description:x.newStatus};
+        });
+
+        // return [
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'},
+        //     {date:'1401/4/5 12:20',description:'سفارش توسط کاربر ثبت شد'}
+        //   ]
       },
       async taghyire_faaliate_gardoone({parameter}){
         let {state,userId} = parameter;
@@ -141,8 +178,8 @@ export default function services(getState,token,userCardCode) {
   }
   return Service({
     services: fn(),
-    baseUrl: 'https://retailerapp.bbeta.ir/api/v1',
-    // baseUrl:'https://localhost:44339/api/v1',
+    // baseUrl: 'https://retailerapp.bbeta.ir/api/v1',
+    baseUrl:'https://localhost:44339/api/v1',
     getState,
     cacheAll: true
   })
